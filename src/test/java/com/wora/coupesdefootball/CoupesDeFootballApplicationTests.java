880c7ec128c4df4e8711c6fb46f9dc360a088364
mohamed <mohamednouaoui5@gmail.com>
2024-12-09 09:28:55 +0100

    all

---
+++ src/test/java/com/wora/coupesdefootball/CoupesDeFootballApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wora.coupesdefootball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoupesDeFootballApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
